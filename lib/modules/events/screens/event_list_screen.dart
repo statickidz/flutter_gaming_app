168062e2644c0ae96a83b6430cf937ef8ee7ccd1
Adrián <statickidz@gmail.com>
2020-12-10 14:52:47 +0100

    chore: improved events page

--- lib/modules/events/screens/event_list_screen.dart
+++ lib/modules/events/screens/event_list_screen.dart
@@ -16,21 +16,95 @@ class _EventListState extends State<EventListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          child: Heading(title: 'Active now'),
-          padding: EdgeInsets.symmetric(horizontal: 15),
-        ),
-        SizedBox(height: 5),
-        Container(
-          child: HomeCarousel(),
-          padding: EdgeInsets.only(left: 10, bottom: 5),
-        ),
-        Container(
-          child: Heading(title: 'Upcoming'),
-          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-        ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            child: Image.asset(
+              'images/card1-notext.png',
+            ),
+            padding: EdgeInsets.only(left: 15, right: 15, bottom: 5),
+          ),
+          Container(
+            child: Heading(title: 'Active now'),
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          ),
+          SizedBox(height: 5),
+          Container(
+            child: HomeCarousel(),
+            padding: EdgeInsets.only(left: 10, bottom: 5),
+          ),
+          Container(
+            child: Heading(title: 'Upcoming'),
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  child: Image.asset(
+                    'images/card1.png',
+                  ),
+                  padding: EdgeInsets.only(left: 15, right: 7.5, bottom: 10),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Image.asset(
+                    'images/card2.png',
+                  ),
+                  padding: EdgeInsets.only(right: 15, left: 7.5, bottom: 10),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  child: Image.asset(
+                    'images/card2.png',
+                  ),
+                  padding: EdgeInsets.only(left: 15, right: 7.5, bottom: 10),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Image.asset(
+                    'images/card1.png',
+                  ),
+                  padding: EdgeInsets.only(right: 15, left: 7.5, bottom: 10),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  child: Image.asset(
+                    'images/card1.png',
+                  ),
+                  padding: EdgeInsets.only(left: 15, right: 7.5, bottom: 10),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Image.asset(
+                    'images/card2.png',
+                  ),
+                  padding: EdgeInsets.only(right: 15, left: 7.5, bottom: 10),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          )
+        ],
+      ),
+      /*children: [
+        
         Expanded(
           child: GridView.count(
             padding: EdgeInsets.symmetric(horizontal: 5),
@@ -46,7 +120,7 @@ class _EventListState extends State<EventListScreen> {
             }),
           ),
         )
-      ],
+      ],*/
     );
   }
 }
